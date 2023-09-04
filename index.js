79087c1a014c06e2af42518efca0d52ba0251df7
chyhhwen <chyhhwen@gmail.com>
2023-09-04 17:04:16 +0800

    test

---
+++ index.js
@@ -0,0 +1,32 @@
+var i = -1;
+showSlides = ()=>
+{
+    var slides = document.querySelectorAll(".slide");
+    if (i > slides.length)
+    {
+        i = 1;
+    }
+    else
+    {
+        i += 1;
+    }
+    slides[i].style.display = "block";
+    if(setTimeout(showSlides, 2000))
+    {
+        slides[i].style.display = "none";
+        if((i+1) == slides.length)
+        {
+            i = -1;
+        }
+        slides[i + 1].style.display = "block";
+    }
+
+}
+window.onload = () =>
+{
+    var slideIndex = 0;
+    showSlides();
+}
+
+
+
